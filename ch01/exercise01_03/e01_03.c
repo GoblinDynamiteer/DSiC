baa9baaa57a24382d0f5fef8a200b0b66ab01e95
Johan Kämpe <kampe.johan@gmail.com>
2017-02-08 16:06:40 +0100

    Exercise 01-03 complete

--- ch01/exercise01_03/e01_03.c
+++ ch01/exercise01_03/e01_03.c
@@ -12,7 +12,9 @@
  */
 
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdbool.h> //bool variable
+#include <stdlib.h> //rand() srand()
+#include <time.h> //time()
 
 /*	Date structure 	*/
 typedef struct{
@@ -22,13 +24,32 @@ typedef struct{
 }date;
 
 enum {YEAR_DAYS = 365, LEAP_YEAR_DAYS};
+enum {
+	JAN = 1,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
+int daysPerMonth[13] = {
+	0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int daysBetween(date d1, date d2);
 bool isLeapYear(int year);
 
 int main(void){
-	date d1 = {12, 11, 1984};
-	date d2 = {13, 9, 2005};
+	srand(time(NULL));
+	date d1,d2;
+	/* Check random dates	*/
+	for(int i = 0; i < 20; i++){
+		d1.year = 1600 + (rand() % 600);
+		d2.year = 1600 + (rand() % 600);
+		d1.month = 1 + (rand() % 12);
+		d2.month = 1 + (rand() % 12);
+		d1.day = 1 + (rand() % daysPerMonth[d1.month]);
+		d2.day = 1 + (rand() % daysPerMonth[d2.month]);
+		printf("There are %d days between "
+			"%04d-%02d-%02d and %04d-%02d-%02d\n",
+			daysBetween(d1, d2),
+			d1.year, d1.month, d1.day,
+			d2.year, d2.month, d2.day
+		);
+	}
 	return 0;
 }
 
@@ -36,7 +57,6 @@ int main(void){
 int getDaysFromYears(int year){
 	int days = 0;
 	while(--year){ //Skip current year
-		printf("Calcing year %d\n", year);
 		if(isLeapYear(year)){
 			days += LEAP_YEAR_DAYS;
 		}
@@ -47,13 +67,25 @@ int getDaysFromYears(int year){
 	return days;
 }
 
+/*	Calculates days from year start. 	*/
 int getDaysFromYearStart(date date){
-	int days = 0;
+	int days = date.day;
+	int month = date.month;
+	while(--month){
+		if(month == FEB && isLeapYear(date.year)){
+			days += daysPerMonth[month] + 1;
+		}
+		else{
+			days += daysPerMonth[month];
+		}
+	}
 	return days;
 }
 
+/*	Gets days between two dates 	*/
 int daysBetween(date d1, date d2){
-	return 123;
+	return (getDaysFromYearStart(d2) + getDaysFromYears(d2.year))
+		- (getDaysFromYearStart(d1) + getDaysFromYears(d1.year));
 }
 
 /*	Determines if year is a leap year 	*/
