94b885724b400248ebce29de6337c90c1ac9fe44
Johan Kämpe <kampe.johan@gmail.com>
2017-02-09 21:37:57 +0100

    Built linked list functions to use in exercises

---
+++ lib/libll.c
@@ -0,0 +1,50 @@
+/*   Integer linked list functions  */
+
+#include "libll.h"
+
+/*  Inserts value last in list   */
+void insLast(node * n, int val){
+  while(!isEmpty(getNext(n))){
+    n = getNext(n);
+  }
+  n->next = createNode(val);
+}
+
+/*  Inserts value first in list,
+    returns pointer to first node   */
+node * insFirst(node * n, int val){
+  if(isEmpty(n)){
+    n = createNode(val);
+  }
+  else{
+    node * temp = n;
+    printf("temp: %d\n", temp->value);
+    n = createNode(val);
+    printf("n: %d\n", n->value);
+    n->next = temp;
+  }
+  return n;
+}
+
+/*  Check if node is empty  */
+bool isEmpty(node * n){
+  return (n == NULL);
+}
+
+/*  Creates a new node, returns pointer   */
+node * createNode(int val){
+  node * newNode = (node *)malloc(sizeof(node));
+  /*  Security check   */
+  if(isEmpty(newNode)){
+    printf("Malloc of new node failed!\n");
+    return NULL;
+  }
+  newNode->value = val;
+  newNode->next = NULL;
+  return newNode;
+}
+
+/*  Returns the next node in the list   */
+node * getNext(node * n){
+  return n->next;
+}
