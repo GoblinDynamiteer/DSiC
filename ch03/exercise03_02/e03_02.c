bbbf11325040d8d18f34f3899e9d6d4335c3844f
Johan Kämpe <kampe.johan@gmail.com>
2017-02-10 11:47:05 +0100

    Exercise 03-02 complete

---
+++ ch03/exercise03_02/e03_02.c
@@ -0,0 +1,59 @@
+/*
+ *  Exercise from book
+ *  Data Structures in C
+ *  by Noel Kalicharan
+ *
+ *	Exercise 02, Chapter 03
+ *  Page 77
+ *
+ *  Write code to reverse the nodes of a linked list.
+ *	Use manipulation of pointer fields only. No new
+ *	nodes must be created!
+ */
+
+#include <stdio.h>
+#include "libll.h"
+
+node * reverseList(node * n);
+void printList(node * top);
+
+int main(void){
+	/*	Add values to list 	*/
+	node *top = createNode(12);
+	for(int i = 1; i < 10; i++){
+		insLast(top, (int)i * 13.75);
+	}
+	/*	Print lists before and after reverse 	*/
+	printf("\nOriginal: \n");
+	printList(top);
+	top = reverseList(top);
+	printf("\nReversed: \n");
+	printList(top);
+	return 0;
+}
+
+/*	Reverse values in list, returns pointer to new top node 	*/
+node * reverseList(node * n){
+	int i = countItems(n);
+	node * newTopNode = getNode(n, i);
+	node * it = newTopNode;
+	while(i){
+		it->next = getNode(n, i);
+		it = getNext(it);
+		i--;
+	}
+	it->next = NULL;
+	return newTopNode;
+}
+
+/*	Printout values in list 	*/
+void printList(node * top){
+	int i = 1;
+	while(1){
+		printf("Node %02d %3d\n",i++, top->value);
+		if(isEmpty(getNext(top))){
+			break;
+		}
+		top = getNext(top);
+	}
+}
